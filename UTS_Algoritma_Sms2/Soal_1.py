077c0e962c64525e9fb31fe03ede5bc1a31e2dde
adindaasa <145623970+adindaasa@users.noreply.github.com>
2024-05-07 16:34:44 +0700

    Add files via upload

---
+++ UTS_Algoritma_Sms2/Soal_1.py
@@ -0,0 +1,11 @@
+a = 30
+b = 10
+
+penjumlahan = a + b
+print('Hasil Penjumlahan :', penjumlahan)
+
+pengurangan = a - b
+print('Hasil Pengurangan :', pengurangan)
+
+modulus = a % b
+print('Hasil Modulus :', modulus)
---
+++ UTS_Algoritma_Sms2/Soal_3.py
@@ -0,0 +1,7 @@
+jumlah_barang = int(input("Masukkan Jumlah Barang : "))
+harga_barang1 = int(input("Masukkan Harga Barang1 : "))
+harga_barang2 = int(input("Masukkan Harga Barang2 : "))
+
+total_harga_barang = harga_barang1+harga_barang2
+
+print("Total Belanjaan : ", total_harga_barang)
